dc7ddbb978c667ff37616b2ecb68863546a301e8
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-07-25 16:13:06 +0530

    Add files via upload

---
+++ Basic_Programs/Characters_Uppercase_Or_Lowercase_else_if_ladder.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         char Ch = '\0';
+
+         printf("\n\n=============*************=============\n");
+
+         printf("\n Enter Your Character = ");
+         scanf("%c",&Ch);
+
+         if(Ch >= 'A' && Ch <= 'Z')
+         {
+               printf("\n %c is Uppercase Letter",Ch);
+         }
+         else if(Ch > 'a' && Ch <= 'z')
+         {
+               printf("\n %c is Lowercase Letter",Ch);
+         }
+         else
+         {
+               printf("\n Invalid Letter");
+         }
+
+         printf("\n\n=============*************=============\n");
+
+         getch();
+
+         printf("\n\t Thanks !!!");
+
+         getch();
+         return 0;
+}
