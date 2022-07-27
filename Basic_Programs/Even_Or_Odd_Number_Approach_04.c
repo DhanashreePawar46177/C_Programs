f780618d64a4962ef22d524fff1a393ce7f344ca
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-07-27 23:15:13 +0530

    Add files via upload

---
+++ Basic_Programs/Even_Or_Odd_Number_Approach_04.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int Num = 0;
+
+         printf("\n================*********================\n");
+
+         Up:
+         printf("\n Enter Your Number = ");
+         scanf("%d",&Num);
+
+         if(Num==0)
+         {
+               printf("\n\t %d is Neutral Number.\n Please Enter Another Number.",Num);
+               system("cls");
+               goto Up;
+         }
+         if(Num%2==0)
+         {
+               printf("\n\t %d is Even",Num);
+         }
+         else
+         {
+               printf("\n\t %d is Odd",Num);
+         }
+
+         printf("\n\n\t Thanks !!!");
+         printf("\n\n================*********================");
+
+         getch();
+         return 0;
+}
