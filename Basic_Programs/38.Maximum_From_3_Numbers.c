d0e8ac635b52089e9d40f6cfa172adc7ad9a399c
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-08-09 18:08:51 +0530

    Add files via upload

---
+++ Basic_Programs/38.Maximum_From_3_Numbers.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int Num1 = 0, Num2 = 0, Num3 = 0;
+         Up:
+
+         printf("\n\n\t===========*******************===========\n");
+
+         printf("\n\t Enter First Integer Number = ");
+         scanf("%d",&Num1);
+
+         printf("\n\t Enter Second Integer Number = ");
+         scanf("%d",&Num2);
+
+         printf("\n\t Enter Third Integer Number = ");
+         scanf("%d",&Num3);
+
+          if(Num1==Num2 && Num2==Num3)
+         {
+             printf("\n\n\t All Numbers Are Equal");
+             getch();
+             system("cls");
+             goto Up;
+         }
+
+
+         if(Num1 > Num2 && Num1 >Num3)
+         {
+               printf("\n\n\t %d is Greater",Num1);
+         }
+         else if(Num2 > Num1 && Num2 > Num3)
+         {
+               printf("\n\n\t %d is Greater",Num2);
+         }
+         else if(Num3 > Num1 && Num3 > Num2)
+         {
+               printf("\n\n\t %d is Greater",Num3);
+         }
+         else
+         {
+               printf("\n\n\t Invalid Numbers");
+         }
+
+         printf("\n\n\t===========*******************===========\n");
+         printf("\n\t\t Thanks !!!\n");
+
+         getch();
+         return 0;
+}
