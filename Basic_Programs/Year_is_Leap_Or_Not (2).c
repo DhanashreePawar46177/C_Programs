6d9df201b6e7b62eb060502acb9fc1897dbb9cc5
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-08-11 21:18:29 +0530

    Add files via upload

---
+++ Basic_Programs/Year_is_Leap_Or_Not (2).c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int Year = 0;
+         
+         printf("\n\t===============******===============\n");
+         
+         printf("\n\t Enter Year = ");
+         scanf(" %d",&Year);
+         
+         if(Year%4 == 0)
+         {
+             printf("\n\t %d is Leap Year",Year);
+         }
+         else
+         {
+             printf("\n\t %d is Not Leap Year",Year);
+         }
+         
+         printf("\n\n\t===============******===============\n");
+         getch();
+         return 0;
+}
