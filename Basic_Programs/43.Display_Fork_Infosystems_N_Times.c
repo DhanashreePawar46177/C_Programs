8f1c3f7dc8ca4f432764c01fcd9bd1464453b1ea
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-08-14 18:41:30 +0530

    Add files via upload

---
+++ Basic_Programs/43.Display_Fork_Infosystems_N_Times.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int No = 0, Cnt = 0;
+
+         printf("\n\t Enter Positive Integer Number For Printing Name Of Class = ");
+         scanf("%d",&No);
+
+         system("cls");
+         printf("\n\n\t============*********===============\n");
+
+         for(Cnt = 1; Cnt <= No; Cnt++)
+         {
+               printf("\n\t\t FORK INFOSYSTEMS\n");
+         }
+
+         printf("\n\n\t============*********===============\n");
+         printf("\n\t\t Thanks !!!\n");
+
+         getch();
+         return 0;
+}
