6bf75c303d331045ce218ada68618c0131d710c4
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-08-16 20:49:03 +0530

    Update 10.Table_Of_Number_while.c

--- Basic_Programs/10.Table_Of_Number_while.c
+++ Basic_Programs/10.Table_Of_Number_while.c
@@ -3,19 +3,19 @@
 
 int main()
 {
-       int No = 0 ,Cnt = 0;
+       int Num = 0 ,Cnt = 0;
 
        printf("\n\n===========******************===========\n");
 
        printf("\n Enter Number To Print Its Table = ");
-       scanf("%d",&No);
+       scanf("%d",&Num);
 
        printf("\n\n===========******************===========\n");
 
        Cnt=1 ;
        while( Cnt<=10)
        {
-             printf("\n\t\t %d",No * Cnt);
+             printf("\n\t\t %d",Num * Cnt);
              Cnt++;
        }
 
