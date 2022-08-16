338f156d750fa23ee59749f41fd77cfb1f637c36
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-08-16 20:50:16 +0530

    Update 11.Table_Of_Number_for.c

--- Basic_Programs/11.Table_Of_Number_for.c
+++ Basic_Programs/11.Table_Of_Number_for.c
@@ -3,18 +3,18 @@
 
 int main()
 {
-       int No = 0 ,Cnt = 0;
+       int Num = 0 ,Cnt = 0;
 
        printf("\n\n===========******************===========\n");
 
        printf("\n Enter Number To Print Its Table = ");
-       scanf("%d",&No);
+       scanf("%d",&Num);
 
        printf("\n\n===========******************===========\n");
 
        for(Cnt = 1 ; Cnt <= 10 ; Cnt++)
        {
-             printf("\n\t\t %d",No * Cnt);
+             printf("\n\t\t %d",Num * Cnt);
        }
 
        printf("\n\n===========*******************===========\n");
