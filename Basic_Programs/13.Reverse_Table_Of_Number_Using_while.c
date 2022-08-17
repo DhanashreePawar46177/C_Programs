834d3e17af44124625d9ed7a24d095d6e8880d90
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-08-17 21:35:12 +0530

    Update 13.Reverse_Table_Of_Number_Using_while.c

--- Basic_Programs/13.Reverse_Table_Of_Number_Using_while.c
+++ Basic_Programs/13.Reverse_Table_Of_Number_Using_while.c
@@ -3,24 +3,24 @@
 
 int main()
 {
-       int No = 0 ,Cnt = 0;
+       int Num = 0 ,Cnt = 0;
 
        printf("\n\n\t=========******************=========\n");
 
        printf("\n\t Enter Number To Print Its Table = ");
-       scanf("%d",&No);
+       scanf("%d",&Num);
 
        printf("\n\n\t=========******************=========\n");
 
        Cnt=10 ;
-       while( Cnt>=1)
+       while( Cnt >= 1)
        {
-             printf("\n\t %d",No * Cnt);
+             printf("\n\t %3d * %3d = %3d",Num,Cnt,Num * Cnt);
              Cnt--;
        }
 
        printf("\n\n\t=========*******************=========\n");
-       printf("\n\t\t Thanks!!!");
+       printf("\n\t\t Thanks!!!\n");
 
        getch();
        return 0;
