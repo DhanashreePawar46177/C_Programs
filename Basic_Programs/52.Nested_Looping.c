ae23d1d1f8a003f06dc85bd534773336053aeb78
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-08-27 20:25:42 +0530

    Add files via upload

---
+++ Basic_Programs/52.Nested_Looping.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int i = 0, j = 0;
+
+        printf("\n *********************************");
+        printf("\n\t Welcome To Class\n");
+
+        for(i = 1 ; i <= 4 ; i++)
+        {
+              for(j = 1 ; j <= 5 ; j++)
+              {
+                    printf("\n\t FORK INFSYSTEMS ");
+              }
+              printf("\n\t   Karad \n");
+        }
+
+        printf("\n *********************************");
+        printf("\n\n\t Thanks !!!");
+
+        getch();
+        return 0;
+}
