1ef69ee1f60aa4d87f6a923b74d589927d2a9798
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-09-03 22:37:53 +0530

    Add files via upload

---
+++ Looping_Programs/20.Printing_India_Using_do_while.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int i = 1;
+
+         printf("\n\n\t=========********=========\n");
+         do
+         {
+              printf("\n\t\t India");
+              i++;
+         }while(i<=5);
+
+         printf("\n\n\t=========********=========\n");
+
+         printf("\n\n\t Thanks !!!\n");
+
+         getch();
+         return 0;
+}
