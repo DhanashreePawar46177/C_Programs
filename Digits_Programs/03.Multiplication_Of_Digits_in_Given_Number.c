70316e0e513d679e331c405aab8219c83d48c5b6
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-09-07 22:03:26 +0530

    Add files via upload

---
+++ Digits_Programs/03.Multiplication_Of_Digits_in_Given_Number.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int No = 0, Dig = 0, dMult = 1, Temp = 0;
+
+         Up:
+         printf("\n Enter Positive Integer Number To Calculate Multiplication  = ");
+         scanf("%d",&No);
+
+         if(No <= 0)
+         {
+               printf("\n Invalid Number. Please Enter Positive Integer Number.");
+               getch();
+               system("cls");
+               goto Up;
+         }
+
+         Temp = No;
+         while(Temp > 0)
+         {
+                Dig =  Temp%10;
+                dMult = dMult* Dig;
+                Temp = Temp/10;
+         }
+
+         printf("\n Multiplication of Digits in Given Number %d is = %d\n",No,dMult);
+
+         printf("\n\n Thanks !!!\n");
+
+         getch();
+         return 0;
+}
