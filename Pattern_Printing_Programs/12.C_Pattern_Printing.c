a35c49756eebfa586adf473ff9f5b650a93b330a
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-09-10 20:12:10 +0530

    Add files via upload

---
+++ Pattern_Printing_Programs/12.C_Pattern_Printing.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int r = 0, c = 0, rCnt = 0, cCnt = 0;
+
+         printf("\n Please Enter Row Count is Greater Than Column Count\n");
+
+         printf("\n Enter Count For Rows = ");
+         scanf("%d",&rCnt);
+
+         printf("\n Enter Count For Columns = ");
+         scanf("%d",&cCnt);
+
+         printf("\n\n Pattern is =>\n\n");
+
+         for(r = 1; r <= rCnt; r++)
+         {
+                  for(c = 1; c <= cCnt; c++)
+                  {
+                           if(r==1&&c>1 || r==rCnt&&c>1 ||c==1&&r>1&&r<rCnt)
+                            {
+                                  printf(" *");
+                            }
+                            else
+                            {
+                                  printf("  ");
+                            }
+                  }
+                  printf("\n");
+         }
+
+         printf("\n\n Thanks !!!\n");
+
+         getch();
+         return 0;
+}
