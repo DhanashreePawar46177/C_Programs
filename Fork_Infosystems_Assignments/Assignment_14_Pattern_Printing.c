cef8342c466ec4801edd245285fa6f601e5d0ff8
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-09-13 22:30:05 +0530

    Fork Infosystems Assignment

---
+++ Fork_Infosystems_Assignments/Assignment_14_Pattern_Printing.c
@@ -0,0 +1,50 @@
+//A B C D E
+//B       B
+//C       C
+//D       D
+//A B C D E
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int r = 0, c = 0, Cnt = 0;
+         char Ch = '\0';
+
+         printf("\n Enter Count Rows and Columns = ");
+         scanf("%d",&Cnt);
+
+         printf("\n\n Pattern is =>\n\n");
+
+         for(r = 1 ; r <= Cnt; r++)
+         {
+                  for(c = 1,Ch = 'A'; c <= Cnt; c++)
+                  {
+                           if(r == 1 || r == Cnt || c == 1 || c == Cnt)
+                           {
+                                 if( r == 1 || r == Cnt)
+                                 {
+                                        printf(" %c ",Ch);
+                                 }
+                                 else
+                                 {
+                                        Ch = 'A';
+                                        Ch = Ch + (r - 1);
+                                        printf(" %c ",Ch);
+                                 }
+                           }
+                           else
+                           {
+                                 printf("   ");
+                           }
+                           Ch++;
+                  }
+
+                  printf("\n");
+         }
+
+         printf("\n\n Thanks !!!\n");
+
+         getch();
+         return 0;
+}
