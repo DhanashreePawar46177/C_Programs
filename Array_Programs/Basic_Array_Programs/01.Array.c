84b7a435315c17de23ecf78dd847a6a73a5314fc
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-09-23 20:33:12 +0530

    Update 01.Array.c

--- Array_Programs/Basic_Array_Programs/01.Array.c
+++ Array_Programs/Basic_Array_Programs/01.Array.c
@@ -5,12 +5,12 @@ int main()
 {
         int Num[6] = {12, 23, 54, 78, 89, 65};
 
-        printf("\n\n The Value Of 0th Number = %d",Num[0]);
-        printf("\n\n The Value Of 1st Number = %d",Num[1]);
-        printf("\n\n The Value Of 2nd Number = %d",Num[2]);
-        printf("\n\n The Value Of 3rd Number = %d",Num[3]);
-        printf("\n\n The Value Of 4th Number = %d",Num[4]);
-        printf("\n\n The Value Of 5th Number = %d",Num[5]);
+        printf("\n\n The Value Of 1st Number = %d",Num[0]);
+        printf("\n\n The Value Of 2nd Number = %d",Num[1]);
+        printf("\n\n The Value Of 3rd Number = %d",Num[2]);
+        printf("\n\n The Value Of 4th Number = %d",Num[3]);
+        printf("\n\n The Value Of 5th Number = %d",Num[4]);
+        printf("\n\n The Value Of 6th Number = %d",Num[5]);
 
         printf("\n\n Thanks !!!\n");
 
