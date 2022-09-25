1cb318f586d784db51ecd48e3f577cad21519920
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-09-25 19:56:43 +0530

    R Pattern Printing

--- Pattern_Printing_Programs/27.R_Pattern_Printing.c
+++ Pattern_Printing_Programs/27.R_Pattern_Printing.c
@@ -5,22 +5,22 @@ int main()
 {
         int r = 0, c = 0, Cnt = 0;
 
-        printf("\n Enter Odd Number Count For Rows and Columns = ");
+        printf("\n Enter Odd Number Count Of Rows and Columns = ");
         scanf("%d",&Cnt);
 
         printf("\n\n Pattern is =>\n\n");
 
-        for(r = 1; r <= Cnt+3; r++)
+        for(r = 1; r <= Cnt+2; r++)
         {
                 for(c = 1; c <= Cnt; c++)
                 {
-                        if(c == 1 || r == 1&& c<Cnt|| c == Cnt && r < (Cnt+1)/2 && r >1 || r == (Cnt+1)/2 && c <Cnt|| r-c== (Cnt+1)/2-1)
+                        if(c == 1 || r == 1 && c < Cnt || c == Cnt && r < (Cnt+1)/2 && r >1 || r == (Cnt+1)/2 && c < Cnt || r-c == ((Cnt+1)/2-1))
                         {
-                                printf(" * ");
+                                printf(" *");
                         }
                         else
                         {
-                                printf("   ");
+                                printf("  ");
                         }
                 }
                 printf("\n");
