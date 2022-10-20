dce03c74ddc66bb389548e0ff61671e00c811ed4
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-10-20 22:42:39 +0530

    String Program

---
+++ String_Programs/03.White_Space_Count_in_Given_String.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        char cSrc[50] = {'\0'};
+        int Cnt = 0, i = 0;
+
+        puts("\n\n Enter a String.");
+        gets(cSrc);
+
+        while(cSrc[i] != '\0')
+        {
+            if(cSrc[i] == ' ')
+            {
+                Cnt++;
+            }
+            i++;
+        }
+
+        printf("\n The White space Count in Given String = %d",Cnt);
+
+        printf("\n\n Thanks !!!\n");
+
+        getch();
+        return 0;
+}
