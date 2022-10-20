dc55f904e228333b0cba520274ef248a47d86ed2
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-10-20 22:36:42 +0530

    String Program

---
+++ String_Programs/01.String_gets_puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        char cSrc[50] = {'\0'};
+
+        puts("\n Enter a String = ");
+        gets(cSrc);
+
+        printf("\n\n Entered String is = %s",cSrc);
+
+        printf("\n\n Thanks !!!\n");
+
+        getch();
+        return 0;
+}
