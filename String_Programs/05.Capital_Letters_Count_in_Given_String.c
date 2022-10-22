c5ab7b46497188afdba8d50da268936feb9388fd
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-10-22 20:15:34 +0530

    String Program

--- String_Programs/05.Capital_Letters_Count_in_Given_String.c
+++ String_Programs/05.Capital_Letters_Count_in_Given_String.c
@@ -6,7 +6,7 @@ int main()
         char cSrc[50] = {'\0'};
         int Cnt = 0, i = 0;
 
-        puts("\n Enter a String.");
+        printf("\n Enter a String = ");
         gets(cSrc);
 
         while(cSrc[i] != '\0')
