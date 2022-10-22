12579ed5f6c043229fd1452c4c9d998990cb734b
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-10-22 18:05:02 +0530

    String Program

---
+++ String_Programs/08.Special_Symbols_Count_in_Given_String.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>>
+
+int main()
+{
+        char cSrc[50] = {'\0'};
+        int Cnt = 0, i = 0;
+
+        puts("\n\n Enter a String.\n");
+        gets(cSrc);
+
+        while(cSrc[i] != '\0')
+        {
+                if(!((cSrc[i] >= 'A'&& cSrc[i] <= 'Z') || (cSrc[i] >= 'a'&& cSrc[i] <= 'z') || (cSrc[i] >= '0'&& cSrc[i] <= '9')))
+                {
+                    Cnt++;
+                }
+                i++;
+        }
+
+        printf("\n The Vowels Count in Given String = %d",Cnt);
+
+        printf("\n\n Thanks !!!\n");
+
+        getch();
+        return 0;
+}
