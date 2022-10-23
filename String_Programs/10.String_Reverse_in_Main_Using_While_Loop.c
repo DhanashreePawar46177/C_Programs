859dea33a355f749ea45340489ee824a713a09a3
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-10-23 20:21:37 +0530

    String Program

---
+++ String_Programs/10.String_Reverse_in_Main_Using_While_Loop.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        char cSrc[20] = {'\0'}, cDest[20] = {'\0'};
+        int i = 0, j = 0;
+
+        printf("\n\n Enter a String  = ");
+        gets(cSrc);
+
+        while(cSrc[i] != '\0')
+        {
+            i++;
+        }
+
+        i = i - 1;
+
+        while(i >= 0)
+        {
+            cDest[j] = cSrc[i];
+            j++;
+            i--;
+        }
+
+        cDest[j] = '\0';
+
+        printf("\n\n=======================================================================");
+
+        printf("\n\n Given String = %s ",cSrc);
+        printf("\n\n Reverse Of Given String is = %s",cDest);
+
+        printf("\n\n=======================================================================");
+        printf("\n\n Thanks !!!\n");
+
+        getch();
+        return 0;
+}
