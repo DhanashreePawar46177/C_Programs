2a4943232fc40a4bc2b761f213567a26bec22b7b
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-11-07 19:31:28 +0530

    Structure Program

---
+++ Structure_Programs/02.Basic_Structure_Program.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct student
+{
+        int Roll_No;
+        char Name[20];
+        char City[9];
+        float Per;
+};
+
+int main()
+{
+        struct student std1, std2, std3 = {120, "Dhanashree","Pune",88.77};
+
+        std1.Roll_No = 101;
+        strcpy(std1.Name,"Dhanu");
+        strcpy(std1.City,"Karad");
+        std1.Per = 78.56;
+
+        printf("\n Enter Roll No = ");
+        scanf("%d",&std2.Roll_No);
+
+        fflush(stdin);
+
+        printf("\n Enter Name = ");
+        gets(std2.Name);
+
+        printf("\n Enter City = ");
+        gets(std2.City);
+
+        fflush(stdin);
+
+        printf("\n Enter Percentage = ");
+        scanf("%f",&std2.Per);
+
+        printf("\n\n Student Details Given are => \n\n Roll No = %d. \n Name = %s. \n City = %s. \n Percentage = %0.2f. ",std1.Roll_No,std1.Name,std1.City,std1.Per);
+        printf("\n\n Student Details Given are => \n\n Roll No = %d. \n Name = %s. \n City = %s. \n Percentage = %0.2f. ",std2.Roll_No,std2.Name,std2.City,std2.Per);
+        printf("\n\n Student Details Given are => \n\n Roll No = %d. \n Name = %s. \n City = %s. \n Percentage = %0.2f. ",std3.Roll_No,std3.Name,std3.City,std3.Per);
+
+        getch();
+        return 0;
+}
