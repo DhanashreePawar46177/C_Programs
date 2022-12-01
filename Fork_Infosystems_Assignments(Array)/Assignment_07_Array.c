d3411832bc870f07df793cad754600ae182cc72a
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2022-12-01 22:47:54 +0530

    Array Assignment

--- Fork_Infosystems_Assignments(Array)/Assignment_07_Array.c
+++ Fork_Infosystems_Assignments(Array)/Assignment_07_Array.c
@@ -43,7 +43,7 @@ int main()
             printf("\n %d is Not Found at any Location",S_Ele);
         }
 
-        printf("\n\n Thanks !!!\n");
+        printf("\n\n Thanks!!!\n");
 
         getch();
         return 0;
