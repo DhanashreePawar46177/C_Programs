c5b99f5d7e6f52162de71e04b5e32295a2f156a9
DhanashreePawar46177 <109866093+DhanashreePawar46177@users.noreply.github.com>
2023-03-13 21:05:42 +0530

    Pointer Program

---
+++ Pointer_Programs/02.Void_Pointer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    void *vptr;
+
+    printf("\n The Size Of the void Pointer = %d",sizeof(vptr));
+
+    printf("\n\n Thanks !!!\n");
+
+    getch();
+    return 0;
+}
